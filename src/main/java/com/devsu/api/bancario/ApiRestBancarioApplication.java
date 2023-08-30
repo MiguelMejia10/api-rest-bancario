d1ff6850c70033cc19ef4e95203a3fcd511b22a9
J. Miguel Mejia Quindales <jorgemiguelfv05@gmail.com>
2023-08-29 23:52:55 -0500

    Cracion del proyecto base con librerias implementadas

---
+++ src/main/java/com/devsu/api/bancario/ApiRestBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.api.bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBancarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestBancarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsu/api/bancario/ApiRestBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsu.api.bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBancarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
